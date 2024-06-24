9e1588ca09a9d669359a6de072ce622473833cb6
suraj sakhare <sakharesuraj10@gmail.com>
2024-06-24 13:14:37 +0530

    learn ArrayList

---
+++ DSA COURSE/Collection Framework/LearnArrayList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class LearnArrayList {
+    public static void main(String[] args) {
+        // ArrayList<String> list=new ArrayList<>();
+        // ArrayList<String> newList=new ArrayList<>();
+        // list.add("rahul");
+        // list.add("raj");
+        // list.add("raj");
+        // list.add(2,"lol");
+        // newList.add("pune");
+        // newList.add("pimpri");
+        // list.addAll(3,newList);
+        // // System.out.println(list.get(0));
+        // list.remove(3);
+        // list.remove(String.valueOf("pune"));
+        // list.set(0, "jadu");
+        // System.out.println(list);
+        // System.out.println( list.contains("lol"));
+
+        // for (int i = 0; i < list.size(); i++) {
+        //     System.out.println(list.get(i));
+        // }
+
+        // for (String string : list) {
+        //     System.out.println(string);
+        // }
+        // Iterator<String> it=list.iterator();
+
+        // while (it.hasNext()) {
+        //     System.out.print(it.next()+" ");
+        // }
+        // list.clear();
+        // System.out.println(list);
+
+    }
+}
